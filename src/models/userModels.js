ee46757e2b7345854e3ac791ad51a2f1a248ee7e
Rahul Mahto <rahulmahto0428@gmail.com>
2025-12-11 09:16:08 +0000

    fixed small error in userModels.js

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String, 
     enum: ["superadmin, editor, user"],
-    default: user,
+    default: "user",
   }
 }, {timestamps: true});
 
